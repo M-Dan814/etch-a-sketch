5fe3c524677b5224acbf5c4f481ae61b61f5e7c6
Dayan <m.dayan5689@gmail.com>
2022-10-06 10:58:16 -0700

    Added Event listener to button
    The Event Listener takes a value upto 100 and sets the grids rows and columns to that number. It then fills up the container div and then adds the same hover effect on every smaller div.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const container = document.querySelector(".container");
+const container = document.querySelector(".container"); 
 
 for (let i = 0; i < 256; i++) {
   const box = document.createElement("div");
@@ -6,10 +6,34 @@ for (let i = 0; i < 256; i++) {
   container.appendChild(box);
 }
 
+const button = document.querySelector("#new-grid");
+button.addEventListener("click", () => {
+  let input = parseInt(prompt("Enter new grid size (Upto 100): "));
+  if (input > 100) {
+    input = parseInt(prompt("Invalid Value. Enter again: "));
+  }
+  container.style["grid-template-rows"] = `repeat(${input}, 1fr)`;
+  container.style["grid-template-columns"] = `repeat(${input}, 1fr)`;
+  container.innerHTML = "";
+  for (let i = 0; i < input * input; i++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    container.appendChild(box);
+  }
+
+  const blocks = document.querySelectorAll(".box");
+  blocks.forEach((block) => {
+    block.addEventListener("mouseover", (e) => {
+      e.target.classList.remove("box");
+      e.target.classList.add("dark");
+    });
+  });
+});
+
 const blocks = document.querySelectorAll(".box");
-blocks.forEach(
-    block => {block.addEventListener("mouseover", (e) => {
-        e.target.classList.remove("box")
-        e.target.classList.add("dark")
-    })}
-)
+blocks.forEach((block) => {
+  block.addEventListener("mouseover", (e) => {
+    e.target.classList.remove("box");
+    e.target.classList.add("dark");
+  });
+});
